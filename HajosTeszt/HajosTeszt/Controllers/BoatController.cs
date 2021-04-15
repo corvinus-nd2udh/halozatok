f6100adaf45c208b28a88347c140f3f99a2316b3
Bence Dosztár <bence.dosztar@stud.uni-corvinus.hu>
2021-04-15 15:07:14 +0200

    Database Connection String, API Controller

---
+++ HajosTeszt/HajosTeszt/Controllers/BoatController.cs
@@ -0,0 +1,25 @@
+﻿using HajosTeszt.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HajosTeszt.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public ActionResult M1()
+        {
+            hajostesztContext context = new hajostesztContext();
+            var kerdesek = from x in context.Questions
+                           select x.QuestionText;
+            return new JsonResult(kerdesek);
+        }
+    }
+}
---
+++ HajosTeszt/HajosTeszt/Controllers/TestController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HajosTeszt.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        [Route("corvinus/szerverido")]
+        public IActionResult M1()
+        {
+            string pontosIdo = DateTime.Now.ToShortTimeString();
+            return new ContentResult
+            {
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain,
+                Content = pontosIdo
+            };
+        }
+
+        [HttpGet]
+        [Route("corvinus/nagybetus/{szoveg}")]
+        public IActionResult M2(string szoveg)
+        {
+            return new ContentResult
+            {
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain,
+                Content = szoveg.ToUpper()
+            };
+        }
+    }
+}
